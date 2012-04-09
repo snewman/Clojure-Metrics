4f0af12cc29919c53f05a01073a2561b8319479f
Sam Newman <sam.newman@gmail.com>
2012-04-09 15:12:08 +0100

    Upgraded to lein2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject metrics-riemann "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.3.0-beta1"]
-                           [hiccup "1.0.0-beta1"]
-                           [com.yammer.metrics/metrics-core "2.1.0"]]
-            :main metrics-riemann.server)
-
+(defproject metrics-riemann/metrics-riemann "0.1.0-SNAPSHOT" 
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [noir "1.3.0-beta1"]
+                 [hiccup "1.0.0-beta1"]
+                 [com.yammer.metrics/metrics-core "2.1.0"]]
+  :main metrics-riemann.server
+  :description "FIXME: write this!")
